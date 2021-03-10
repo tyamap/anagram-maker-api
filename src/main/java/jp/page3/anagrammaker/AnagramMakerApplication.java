098867bd5b2d81bccccb4da91082964d03d70036
tyamap <tom333my@outlook.com>
2021-03-10 22:36:26 +0900

    init

---
+++ src/main/java/jp/page3/anagrammaker/AnagramMakerApplication.java
@@ -0,0 +1,13 @@
+package jp.page3.anagrammaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnagramMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnagramMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/page3/anagrammaker/AnagramMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.page3.anagrammaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnagramMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
